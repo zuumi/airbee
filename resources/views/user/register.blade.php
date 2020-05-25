5ffa17b7a4e2a83ae27903147854c803270914eb
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-05-25 16:43:00 +0900

    Enhanced design

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -12,91 +12,85 @@
       <h1>登録フォーム</h1>
       <hr>
       <form action="./toroku" method="post">
-            @csrf
-  			<div class="form-group row">
-    			<label for="familyname"familyname class="col-sm-2 col-form-label">苗字:</label>
-    			<div class="col-sm-4">
-             <input type="text" class="form-control" id="familyname" name="familyname" value="{{old('familyname')}}">
-             @error('familyname')
-             <p>{{$message}}</p>
-             @enderror
-      			</div>
-  			</div>
-  			<div class="form-group row">
-    			<label for="firstname" class="col-sm-2 col-form-label">名前:</label>
-    			<div class="col-sm-4">
-             <input type="text" class="form-control" id="firstname" name="firstname" value="{{old('firstname')}}">
-             @error('firstname')
-             <p>{{$message}}</p>
-             @enderror
-        	</div>
-  			</div>
-  			<div class="form-group row">
-    			<label for="postal" class="col-sm-2 col-form-label">郵便番号:</label>
-    			<div class="col-sm-4">
-             <input type="text" class="form-control" name="postal" value="{{old('postal')}}">
-             @error('postal')
-             <p>{{$message}}</p>
-             @enderror
-        	</div>
-  			</div>
-  			<div class="form-group row">
-    			<label for="address" class="col-sm-2 col-form-label">住所:</label>
-    			<div class="col-sm-4">
-             <input type="text" class="form-control" name="address" value="{{old('address')}}">
-             @error('address')
-             <p>{{$message}}</p>
-             @enderror
-        	</div>
-  			</div>
-  			<div class="form-group row">
-    			<label for="tel" class="col-sm-2 col-form-label">電話番号:</label>
-    			<div class="col-sm-4">
-            <input type="text" class="form-control" name="tel" value="{{old('tel')}}">
-            @error('tel')
+           @csrf
+           <div class="form-group row">
+               <label for="familyname"familyname class="col-sm-2 col-form-label">会員の苗字:</label>
+               <div class="col-sm-4">
+            <input type="text" class="form-control" id="familyname" name="familyname" value="{{old('familyname')}}">
+            @error('familyname')
             <p>{{$message}}</p>
             @enderror
-          </div>
-  			</div>
-  			<div class="form-group row">
-    			<label for="email" class="col-sm-2 col-form-label">Eメールアドレス:</label>
-    			<div class="col-sm-4">
-            				<input type="email" class="form-control"  name="email" value="{{old('email')}}">
-                    @error('email')
-                    <p>{{$message}}</p>
-                    @enderror
-          </div>
-  			</div>
-  			<div class="form-group row">
-    			<label for="birthday" class="col-sm-2 col-form-label">生年月日:</label>
-    			<div class="col-sm-4">
-              <input type="date" class="form-control" id="birthday" name="birthday" value="{{old('birthday')}}">
-              @error('birthday')
-              <p>{{$message}}</p>
-              @enderror
-          	</div>
-  			</div>
-  			<div class="form-group row">
-    			<label for="password" class="col-sm-2 col-form-label">パスワード：</label>
-    			<div class="col-sm-4">
-      				<input type="password" class="form-control" id="password" name="password" value="{{old('password')}}">
-              @error('password')
-              <p>{{$message}}</p>
-              @enderror
-        	</div>
-  			</div>
-  			<div class="form-group row">
-                <div class="col-sm-4">
-                </div>
-                <div class="col-sm-4">
-          			<button type="submit" class="btn btn-primary">登録</button>
-        		</div>
-                <div class="col-sm-4">
-                </div>
-  			</div>
-		</form>
-   </form>
-  </div>
+               </div>
+           </div>
+           <div class="form-group row">
+               <label for="firstname" class="col-sm-2 col-form-label">会員の名前:</label>
+               <div class="col-sm-4">
+            <input type="text" class="form-control" id="firstname" name="firstname" value="{{old('firstname')}}">
+            @error('firstname')
+            <p>{{$message}}</p>
+            @enderror
+           </div>
+           </div>
+           <div class="form-group row">
+               <label for="postal" class="col-sm-2 col-form-label">会員の郵便番号:</label>
+               <div class="col-sm-4">
+            <input type="text" class="form-control" name="postal" value="{{old('postal')}}">
+            @error('postal')
+            <p>{{$message}}</p>
+            @enderror
+           </div>
+           </div>
+           <div class="form-group row">
+               <label for="address" class="col-sm-2 col-form-label">会員の住所:</label>
+               <div class="col-sm-4">
+            <input type="text" class="form-control" name="address" value="{{old('address')}}">
+            @error('address')
+            <p>{{$message}}</p>
+            @enderror
+           </div>
+           </div>
+           <div class="form-group row">
+               <label for="tel" class="col-sm-2 col-form-label">会員の電話番号:</label>
+               <div class="col-sm-4">
+           <input type="text" class="form-control" name="tel" value="{{old('tel')}}">
+           @error('tel')
+           <p>{{$message}}</p>
+           @enderror
+         </div>
+           </div>
+           <div class="form-group row">
+               <label for="email" class="col-sm-2 col-form-label">会員のEメールアドレス:</label>
+               <div class="col-sm-4">
+                           <input type="email" class="form-control"  name="email" value="{{old('email')}}">
+                   @error('email')
+                   <p>{{$message}}</p>
+                   @enderror
+         </div>
+           </div>
+           <div class="form-group row">
+               <label for="birthday" class="col-sm-2 col-form-label">会員の生年月日:</label>
+               <div class="col-sm-4">
+             <input type="date" class="form-control" id="birthday" name="birthday" value="{{old('birthday')}}">
+             @error('birthday')
+             <p>{{$message}}</p>
+             @enderror
+           </div>
+           </div>
+           <div class="form-group row">
+               <label for="password" class="col-sm-2 col-form-label">会員のパスワード：</label>
+               <div class="col-sm-4">
+                   <input type="password" class="form-control" id="password" name="password" value="{{old('password')}}">
+             @error('password')
+             <p>{{$message}}</p>
+             @enderror
+           </div>
+           </div>
+           <div class="form-group row">
+               <div class="col-sm-4">
+                   <button type="submit" class="btn btn-primary">登録</button>
+               </div>
+           </div>
+       </form>
 </div>
   @endsection
 
--- resources/views/user/welcome.blade.php
+++ resources/views/user/welcome.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
         <!-- Styles -->
         <style>
@@ -58,22 +59,50 @@
                 text-transform: uppercase;
             }
 
+            .wrap{
+                width: 100%;
+                height: 100vh;
+            }
+
+            #bg-video{
+                position: absolute; /*positionプロパティを指定*/
+                right: 0;
+                top: 0;
+                bottom:0;
+                z-index: 0; /*z-indexプロパティを指定*/
+                min-width: 100%;
+                height: 100%;
+                opacity:0.7;
+            }
+
             .m-b-md {
                 margin-bottom: 30px;
+                color:black;
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            <div class="top-right links">
-                    <a href="{{ url('/admin/login') }}">管理者画面</a>
-                    <a href="{{ url('/user/auth') }}">会員画面</a>
-            </div>
-            <div class="content">
-                <div class="title m-b-md">
-                    Airbee
+        <div class="jumbotron jumbotron-fluid mb-0 wrap">
+            <!--背景用の動画ファイル-->
+            <video id="bg-video" autoplay loop muted>
+                <source src="{{ asset('/assets/video/Hotel.mp4') }}" />
+            </video>
+            <div class="container flex-center position-ref full-height">
+                <div class="top-right links">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item"><a class="nav-link" style="margin-right: 3px;background:black ;color:white;" href="{{ url('/admin/login') }}">管理者画面</a></li>
+                        <li class="nav-item"><a class="nav-link" style="background:black ;color:white;" href="{{ url('/user/login') }}">会員画面</a></li>
+                    </ul>
+                </div>
+                <div class="content">
+                    <div class="title m-b-md" style="color:black;">
+                        Airbee
+                    </div>
                 </div>
             </div>
         </div>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
 </html>
