3fd6cbc592df9c114b3717f90aa752b17e4e63d4
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-05-26 14:55:17 +0900

    admin menu bar

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -1,47 +1,63 @@
 @extends('layouts.app')
-
+<!-- 河住　更新 0526 -->
+<style media="screen">
+    .container{
+        margin-top:16px;
+    }
+</style>
 @section('title','Show Users')
 
 @section('menubar')
     @parent
     メニュー
-    <table>
-        <tr>
-            <!-- 河住圭紀 更新部分 start -->
-            <td><a href="{{ url('/admin/userlist') }}">会員情報を操作</a></td>
-            <td><a href="{{ url('/inn/show') }}">宿情報を操作</a></td>
-            <!-- 河住圭紀 更新部分 end -->
+    <!-- 河住　更新 0526 -->
+    <ul class="nav nav-tabs">
+        <li class="nav-item"><a href="{{ url('/admin/userlist') }}" class="nav-link active">会員一覧</a></li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">宿情報を操作</a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="{{ url('/inn/show') }}">宿一覧</a>
+                <a class="dropdown-item" href="{{ url('/inn/add') }}">宿登録</a>
+            </div>
+        </li>
+        <li>
             <form action="/admin/usershow" method="get">
-                @csrf
-                <td><input type="text" name="keyword" value="{{ old('keyword')}}"placeholder="キーワードを入力してください。"></td>
-                <td><input type="submit" value="検索"></td>
+                    @csrf
+                    <div class="input_wrap">
+                        <td><input type="text" name="keyword" value="{{ old('keyword')}}"placeholder="キーワードを入力してください。"></td>
+                        <td><input type="submit" value="検索"></td>
+                    </div>
             </form>
-        </tr>
-    </table>
+        </li>
+    </ul>
+    <!--  -->
 @endsection
 
 @section('content')
-<table border="1">
-    <tr>
-        <th>名前</th><th>郵便番号</th>
-        <th>住所</th><th>電話番号</th>
-        <th>Eメールアドレス</th>
-        <th>生年月日</th><th></th><th></th>
-    </tr>
-    @foreach($lists as $li)
+<!-- 河住　更新 0526 -->
+<div class="container">
+<!--  -->
+    <table border="1">
         <tr>
-            <td>{{$li->familyname}}{{$li->firstname}}</td>
-            <td>{{$li->postal}}</td><td>{{$li->address}}</td>
-            <td>{{$li->tel}}</td><td>{{$li->email}}</td>
-            <td>{{$li->birthday}}</td>
-            <!-- 河住圭紀　更新　start 0522 -->
-            <td><a href="/admin/user/edit/{{$li->id}}">更新</a></td>
-            <td><a href="/admin/user/del/{{$li->id}}">削除</a></td>
-            <!-- 河住圭紀　更新　end 0522 -->
+            <th>名前</th><th>郵便番号</th>
+            <th>住所</th><th>電話番号</th>
+            <th>Eメールアドレス</th>
+            <th>生年月日</th><th></th><th></th>
         </tr>
-    @endforeach
-</table>
-
+        @foreach($lists as $li)
+            <tr>
+                <td>{{$li->familyname}}{{$li->firstname}}</td>
+                <td>{{$li->postal}}</td><td>{{$li->address}}</td>
+                <td>{{$li->tel}}</td><td>{{$li->email}}</td>
+                <td>{{$li->birthday}}</td>
+                <!-- 河住圭紀　更新　start 0522 -->
+                <td><a href="/admin/user/edit/{{$li->id}}">更新</a></td>
+                <td><a href="/admin/user/del/{{$li->id}}">削除</a></td>
+                <!-- 河住圭紀　更新　end 0522 -->
+            </tr>
+        @endforeach
+    </table>
+</div>
 @endsection
 
 @section('footer')
--- resources/views/inn/index.blade.php
+++ resources/views/inn/index.blade.php
@@ -1,16 +1,26 @@
 @extends('layouts.app')
-
+<!-- 河住 更新 0526  -->
+<style media="screen">
+    .container{
+        margin-top: 16px;
+    }
+</style>
 @section('title','Index')
 
 @section('menubar')
     @parent
     メニュー
-    <!-- 河住圭紀 更新部分 start -->
-    <table>
-        <tr>
-            <td><a href="{{ url('/admin/userlist') }}">会員情報を操作</a></td>
-            <td><a href="{{ url('/inn/show') }}">宿情報を操作</a></td>
-            <td>
+    <!-- 河住 更新部分 0526 start -->
+    <ul class="nav nav-tabs">
+        <li class="nav-item"><a href="{{ url('/admin/userlist') }}" class="nav-link">会員一覧</a></li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle active" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">宿情報を操作</a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="{{ url('/inn/show') }}">宿一覧</a>
+                <a class="dropdown-item" href="{{ url('/inn/add') }}">宿登録</a>
+            </div>
+        </li>
+        <li>
             <form action="/inn/show" method="post">
                 @csrf
                 <div class="input_wrap">
@@ -18,38 +28,39 @@
                     <input type="submit" value="検索">
                 </div>
             </form>
-            <td>
-        </tr>
-    </table>
-    <!-- 河住圭紀 更新部分 end -->
+        </li>
+    </ul>
+    <!-- 河住 更新 0526 end -->
 @endsection
 
 @section('content')
 <!-- 会員とメニューの見た目を揃えるため、menubarに移しました。 -->
-
-  <table border="1">
-    <tr>
-      <th>宿ID</th><th>宿名</th><th>郵便番号</th><th>住所</th>
-      <th>チェックイン時間</th><th>チェックアウト時間</th><th>分類コード</th>
-      <th>Eメール</th><th></th><th></th>
-    </tr>
-  @foreach ($items as $item)
-    <tr>
-      <td>{{$item->id}}</td>
-      <td>{{$item->name}}</td>
-      <td>{{$item->postal}}</td>
-      <td>{{$item->address}}</td>
-      <td>{{$item->checkintime}}</td>
-      <td>{{$item->checkouttime}}</td>
-      <td>{{$item->code}}</td>
-      <td>{{$item->mail}}</td>
-      <!-- 河住圭紀　追加　start -->
-      <td><a href="/inn/edit/{{$item->id}}">更新</a></td>
-      <td><a href="/inn/del/{{$item->id}}">削除</a></td>
-      <!-- 河住圭紀　追加　end -->
-    </tr>
-  @endforeach
+<!-- 河住　更新　0526 -->
+<div class="container">
+    <table border="1">
+        <tr>
+          <th>宿ID</th><th>宿名</th><th>郵便番号</th><th>住所</th>
+          <th>チェックイン時間</th><th>チェックアウト時間</th><th>分類コード</th>
+          <th>Eメール</th><th></th><th></th>
+        </tr>
+      @foreach ($items as $item)
+        <tr>
+          <td>{{$item->id}}</td>
+          <td>{{$item->name}}</td>
+          <td>{{$item->postal}}</td>
+          <td>{{$item->address}}</td>
+          <td>{{$item->checkintime}}</td>
+          <td>{{$item->checkouttime}}</td>
+          <td>{{$item->code}}</td>
+          <td>{{$item->mail}}</td>
+          <!-- 河住圭紀　追加　start -->
+          <td><a href="/inn/edit/{{$item->id}}">更新</a></td>
+          <td><a href="/inn/del/{{$item->id}}">削除</a></td>
+          <!-- 河住圭紀　追加　end -->
+        </tr>
+      @endforeach
   </table>
+</div>
 @endsection
 
 @section('footer')
