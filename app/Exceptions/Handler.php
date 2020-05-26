1b9bee1ae82a043a447aea63d113b21da4ede647
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-05-26 10:59:31 +0900

    logout error display

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,6 +50,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if($exception instanceof \Illuminate\Session\TokenMismatchException )
+        {
+            return redirect('/user/login');
+        }
         return parent::render($request, $exception);
     }
 }
