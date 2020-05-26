dae794fbd54d84153682e6167debb5a0cd62d276
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-05-26 17:00:21 +0900

    menu fixed

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,21 +11,22 @@
 @section('menubar')
     @parent
     <div class="menus">
-        <table>
-            <tr>
-                <th>メニュー</th>
-            </tr>
-            <tr>
-                <td></td><td><a href="/admin/userlist">会員情報を操作</a></td>
-                <td><a href="/inn/show">宿情報を操作</a></td>
-                <td><a id="logout" href="/admin/logout">ログアウト</a></td>
-            </tr>
-        </table>
+        <ul class="nav nav-tabs">
+            <li class="nav-item"><a href="{{ url('/admin/userlist') }}" class="nav-link">会員一覧</a></li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">宿情報を操作</a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="{{ url('/inn/show') }}">宿一覧</a>
+                    <a class="dropdown-item" href="{{ url('/inn/add') }}">宿登録</a>
+                </div>
+            </li>
+            <li class="nav-item"><a id="logout" href="/admin/logout" class="nav-link">ログアウト</a></li>
+        </ul>
     </div>
 @endsection
 
 @section('content')
-
+    <h3 style="margin-top:16px; text-align:center;">ようこそ、管理者ページへ</h3>
 @endsection
 
 @section('footer')
